9be6ffa5051243fcc32bb83de1e2391d4073098a
Flomaster03 <92091652+Flomaster03@users.noreply.github.com>
2022-03-24 23:32:21 +0300

    Update MonetTransfer.kt

--- src/main/kotlin/MonetTransfer.kt
+++ src/main/kotlin/MonetTransfer.kt
@@ -2,8 +2,8 @@ fun main() {
 
     val amount = 5000_00
 
-    val commissionDouble = if (amount > 35_00) amount * 0.75 / 100 else 35_00
-    val commission = commissionDouble.toInt()
+    val commissionDouble = amount * 0.75 / 100 
+    val commission = if(commissionDouble > 35_00.0) commissionDouble.toInt() else 35_00
     val commissionRuble = commission / 100
     val commissionPenny = commission % 100
 
